6dd5242268532181ada779cfcef2d5c512993ff8
tgparkk <sttgpark@gmail.com>
2026-02-18 12:43:02 +0900

    "revert_physics_changes_restore_working_state"

--- scripts/main.gd
+++ scripts/main.gd
@@ -141,7 +141,6 @@ func _create_ball_scene() -> PackedScene:
 	body.name = "Ball"
 	body.set_script(load("res://scripts/ball.gd"))
 	body.gravity_scale = 1.0
-	body.can_sleep = false
 	body.contact_monitor = true
 	body.max_contacts_reported = 8
 	
